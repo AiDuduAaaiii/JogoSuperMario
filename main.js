7b89bc0b839f5c26cbc554dc0d80ca810a9e27f3
AiDuduAaaiii <169045870+AiDuduAaaiii@users.noreply.github.com>
2024-09-23 11:13:33 -0300

    .

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
-const clouds = document.querySelector(".clouds");
+const clouds = document.querySelector(".nuvem");
 const startButton = document.querySelector(".start");
 const gameOverScreen = document.querySelector(".game-over");
 const scoreElement = document.querySelector(".score");
 
-const audioStart = new Audio("./sound/audio_theme.mp3");
-const gameOverSound = new Audio("./sound/audio_gameover.mp3");
+const audioStart = new Audio("sound/audio_theme.mp3");
+const gameOverSound = new Audio("sound/audio_gameover.mp3");
                                 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
     gameStarted = true;
+    audioStart.play();
 
     pipe.style.animation = "pipe-animation 1.5s infinite linear";
+    
     startButton.style.display = "none";
     mario.style.opacity = "1";
     pipe.style.opacity = "1";
     clouds.style.opacity = "1";
 }
+
 const jump = () => {
     if(gameStarted) {
         mario.classList.add("jump");
@@ -30,7 +34,12 @@ const jump = () => {
     }
 }
 
-    const loop = setInterval (() => {
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+}
+
+const loop = setInterval (() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
@@ -44,9 +53,20 @@ const jump = () => {
         mario.src = "img/game-over.png";
         mario.style.width = "75px";
         mario.style.marginLeft = "50px";
+        audioStart.pause();
 
+        gameOverSound.play();
+
+        gameOverScreen.style.display = "flex";
         clearInverval(loop);
+    } else if (pipePosition < 0 && gameStarted) {
+        updateScore();
+        pipe.style.left = "";
     }
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+const restartGame = () => {
+    window.location.reload();
+}
